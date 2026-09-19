98c1cf4cb47455da81a3ede2b0f24951c7572b1d
Phan Hai Anh <haianh2504077@gmail.com>
2026-09-19 15:13:41 +0700

    Complete basic controller for products

--- src/main/java/product/dto/request/DecreaseProductQuantityRequest.java
+++ src/main/java/product/dto/request/DecreaseProductQuantityRequest.java
@@ -3,7 +3,7 @@ package product.dto.request;
 import jakarta.validation.constraints.Positive;
 
 public class DecreaseProductQuantityRequest {
-    @Positive(message="The decreased quantity cannot be negative")
+    @Positive(message="The decreased quantity must be greater than zero")
     private int decreaseQuantity;
     public int getDecreaseQuantity() {
         return decreaseQuantity;
